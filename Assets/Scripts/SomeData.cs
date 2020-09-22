081cb4363127a04a3c3155bbe40abab8213c1847
Thomas Barembaum <thomasb@voodoo.io>
2020-09-22 17:43:04 -0400

    Creating new prefab variant definitely creates a new FileID

--- Assets/Scripts/SomeData.cs
+++ Assets/Scripts/SomeData.cs
@@ -8,4 +8,5 @@ public class SomeData : ScriptableObject
     [SerializeField] private GameObject somePrefab3 = null;
     [SerializeField] private GameObject somePrefab4 = null;
     [SerializeField] private GameObject masterPrefabCopy = null;
+    [SerializeField] private GameObject otherVariantFromMaster = null;
 }
