1941e8885fbb2010e08a6f81094257d71853ccab
Thomas Barembaum <thomasb@voodoo.io>
2020-09-22 18:37:01 -0400

    renaming fields so that it's clearer

--- Assets/Scripts/SomeData.cs
+++ Assets/Scripts/SomeData.cs
@@ -3,10 +3,11 @@
 [CreateAssetMenu(fileName = "some_data", menuName = "Data/Some")]
 public class SomeData : ScriptableObject
 {
-    [SerializeField] private GameObject somePrefab = null;
-    [SerializeField] private GameObject somePrefab2 = null;
-    [SerializeField] private GameObject somePrefab3 = null;
-    [SerializeField] private GameObject somePrefab4 = null;
+    [SerializeField] private GameObject masterPrefab = null;
+    [SerializeField] private GameObject variantFromMaster = null;
+    [SerializeField] private GameObject variantCopy1 = null;
+    [SerializeField] private GameObject variantCopy2 = null;
+    [SerializeField] private GameObject variantCopy3 = null;
     [SerializeField] private GameObject masterPrefabCopy = null;
     [SerializeField] private GameObject otherVariantFromMaster = null;
 }
